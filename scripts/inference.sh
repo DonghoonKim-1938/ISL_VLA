1c0ea79d123d6dcb7cc4beb74d9ba7584ab55334
uhnam <01unghui@gmail.com>
2025-08-05 14:08:12 +0900

    [ADD] added inference.sh

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -8,12 +8,15 @@ CONDA_ENV_NAME="lerobot"
 BASELINE="pi0"
 DATA_ROOT_DIR="piper_open_the_pot"
 
+BASELINE_PATH=/ckpt/pi0
+ADAPTER_PATH=/result/pi0_qlora_openthepot/checkpoints/005000/pretrained_model
+
 CUDA_VISIBLE_DEVICES=${DEVICES} \
   ${CONDA_ENV_PATH} run -n ${CONDA_ENV_NAME} \
-    --policy.path=/ckpt/pi0 \
-    --adapter_path=/result/pi0_qlora_openthepot/checkpoints/005000/pretrained_model
-    --use_qlora=true
+    --policy.path=${BASELINE_PATH} \
+    --adapter_path=${ADAPTER_PATH} \
+    --use_qlora=true \
     --train_dataset.repo_id="/datasets/${DATA_ROOT_DIR}/lerobot_5hz" \
     --train_dataset.root="/datasets/${DATA_ROOT_DIR}/lerobot_5hz" \
-    --use_devices=true
+    --use_devices=true \
     --target_keywords='["q_proj","k_proj","v_proj"]' \
