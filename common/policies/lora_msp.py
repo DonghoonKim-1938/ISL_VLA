c47011f27bd7eeb283e1bfb7559fe73df53fda78
dh <kdh7344@gmail.com>
2025-08-28 00:55:44 +0900

    [ADD] Centering and Sharpening

--- common/policies/lora_msp.py
+++ common/policies/lora_msp.py
@@ -4,6 +4,7 @@ from typing import Optional, Dict, Callable, Iterable, Tuple, List
 import math
 
 import torch
+import torch.distributed as dist
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -44,6 +45,10 @@ class LoraMSPLinear(LoraLinear):
         self.router = nn.Linear(in_f, cfg.num_experts * cfg.r, bias=False, dtype=base.weight.dtype)
         nn.init.kaiming_uniform_(self.router.weight, a=math.sqrt(5))
 
+        self.register_buffer("router_logit_ma", torch.zeros(self.router.weight.shape[-1], dtype=self.router.weight.dtype, device=self.router.weight.device))
+        self.momentum = 0.99
+        self.temperature = 0.07
+
         self.dropout = nn.Dropout(cfg.dropout) if cfg.dropout > 0.0 else nn.Identity()
 
         # expose merge flag similar to LoRA
@@ -58,8 +63,8 @@ class LoraMSPLinear(LoraLinear):
         x_dp = self.dropout(x)
 
         # Router logits + gates
-        router_logits = self.router(x_dp)  # (..., E)
-        router_logits = F.relu(router_logits)  # (..., E)
+        router_logits = self.router(x_dp) - self.router_logit_ma
+        router_logits = F.relu(router_logits / self.temperature)  # (..., E)
 
         Sigma = torch.diag_embed(router_logits)
 
@@ -72,8 +77,26 @@ class LoraMSPLinear(LoraLinear):
         lora_out = torch.matmul(proj_r.squeeze(-2), B_t)  # (B, S, out)
 
         lora_out = lora_out * self.cfg.scale
-
+        self.update_router_ema(router_logits)
         return base_out + lora_out
+
+    # ---------------------------------------------------------
+    # External update of router logits moving average
+    # ---------------------------------------------------------
+    @torch.no_grad()
+    def update_router_ema(self, router_logits: torch.Tensor):
+
+        batch_sum = router_logits.sum(dim=0)
+        
+        if dist.is_available() and dist.is_initialized():
+            dist.all_reduce(batch_sum)
+            world_size = dist.get_world_size()
+        else:
+            world_size = 1
+
+        batch_mean = batch_sum / (router_logits.size(0) * world_size)
+
+        self.router_logit_ma.mul_(self.momentum).add_(batch_mean * (1 - self.momentum))
 
     def load_adapter_as_expert(
             self,
